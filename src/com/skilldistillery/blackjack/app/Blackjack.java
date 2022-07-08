b5f9b57fb51003434a5954a38dc797efe20b92d0
qdzc123 <zmffhqk@gmail.com>
2022-07-08 18:38:20 -0500

    important descriptive message about the changes made

---
+++ src/com/skilldistillery/blackjack/app/Blackjack.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.blackjack.app;
+
+public class Blackjack {
+	public static void main(String[] args) {
+		Blackjack app = new Blackjack();
+		app.launch();
+	}
+
+	private void launch() {
+		// TODO Auto-generated method stub
+		
+	}
+}
